abecf04b163defdc5100631f986c615285d436dd
byt3-m3 <cbaxtertech@gmail.com>
2021-09-24 09:10:51 -0400

    Initial Commit

---
+++ lib/cloudwatch_utils.py
@@ -0,0 +1,27 @@
+import boto3
+
+
+def get_cloudwatch_client():
+    return boto3.client('cloudwatch')
+
+
+def create_dimension(name, value):
+    return {
+        'Name': name,
+        'Value': value
+    }
+
+
+def update_metric(client, name_space, metric_name, dimensions, value=None, values=None):
+    if value:
+        return client.put_metric_data(
+            Namespace=name_space,
+            MetricData=[
+                {
+                    'MetricName': metric_name,
+                    'Dimensions': dimensions,
+                    'Value': int(value)
+
+                },
+            ]
+        )
