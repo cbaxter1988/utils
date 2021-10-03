3cec5f0a488d7502891129ed412fb71ed3984e1c
byt3-m3 <cbaxtertech@gmail.com>
2021-10-02 21:27:43 -0400

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cbaxter1988_utils',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/byt3-m3/utils',
     description='A Package containing my utils',
     author='Courtney S Baxter Jr',
