df9575a2f8fe58f3e133398d0df4b92e818fa123
byt3-m3 <cbaxtertech@gmail.com>
2021-11-11 20:07:43 -0500

    Cleaned up imports

--- cbaxter1988_utils/requests_utils.py
+++ cbaxter1988_utils/requests_utils.py
@@ -1,9 +1,4 @@
-import http
-import json
-from typing import Union
-
 import requests
-from cbaxter1988_models.src.http_response_models import ProblemDetailModel, PROBLEM_DETAIL_HEADER_JSON
 
 
 def get_html_page(url) -> str:
@@ -17,5 +12,3 @@ def get_request(url):
 def post_request(url, body):
     response = requests.post(url=url, json=body, headers={"Content-Type": "application/json"})
     return response
-
-
