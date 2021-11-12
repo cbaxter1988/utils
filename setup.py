99ee58763f202c2aaec0319bc5f8f19dd16ebd33
byt3-m3 <cbaxtertech@gmail.com>
2021-11-11 20:07:56 -0500

    Upgraded Version After Change

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='cbaxter1988_utils',
-    version='0.2.7',
+    version='0.2.8',
     url='https://github.com/cbaxter1988/utils.git',
     description='A Package containing my utils',
     author='Courtney S Baxter Jr',
